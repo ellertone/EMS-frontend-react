bcfdf8afde4fb42490e781752781500300805127
Ellertone Manasses <123564653+ellertone@users.noreply.github.com>
2024-06-11 13:39:58 +0300

    Dash Board

---
+++ src/data/Data.js
@@ -0,0 +1,40 @@
+import { tokens } from "../theme";
+
+export const attendeesData = 
+[
+    {
+        "AttendeeId": 1,
+        "Name": "John Doe",
+        "Email": "john.doe@example.com",
+        "Phone": "123-456-7890",
+        "EventId": 101
+    },
+    {
+        "AttendeeId": 2,
+        "Name": "Jane Smith",
+        "Email": "jane.smith@example.com",
+        "Phone": "098-765-4321",
+        "EventId": 102
+    },
+    {
+        "AttendeeId": 3,
+        "Name": "Alice Johnson",
+        "Email": "alice.johnson@example.com",
+        "Phone": "555-987-6543",
+        "EventId": 103
+    },
+    {
+        "AttendeeId": 4,
+        "Name": "Bob Brown",
+        "Email": "bob.brown@example.com",
+        "Phone": "555-123-4567",
+        "EventId": 104
+    },
+    {
+        "AttendeeId": 5,
+        "Name": "Charlie Davis",
+        "Email": "charlie.davis@example.com",
+        "Phone": "555-111-2222",
+        "EventId": 101
+    }
+]
